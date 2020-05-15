025030cf246db00b1f5c55d3b9bfe08034c8478d
srishti1798 <65332325+srishti1798@users.noreply.github.com>
2020-05-15 12:10:09 +0530

    15-05-2020

---
+++ Srishti 12-05/Q4.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h> 
+#include <string.h>
+{ 
+    int l = 0; 
+    int h = strlen(str) - 1; 
+    while (h > l) 
+    { 
+        if (str[l++] != str[h--]) 
+        { 
+            printf("%s is Not Palindrome", str); 
+            return; 
+        } 
+    } 
+    printf("%s is palindrome", str); 
+} 
+int main() 
+{ 
+    isPalindrome("abba"); 
+    isPalindrome("abbccbba"); 
+    isPalindrome("geeks"); 
+    return 0; 
+}
